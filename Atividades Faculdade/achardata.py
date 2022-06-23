d86f00083d8cb0a692fcd66f9721c8bc5141c25f
Lucas Mateus <lmateus1067@outlook.com>
2022-06-23 17:05:04 -0300

    Adcionando o modo classico

--- Atividades Faculdade/achardata.py
+++ Atividades Faculdade/achardata.py
@@ -18,3 +18,16 @@ if datan == 5 or datan == 6:
     print(f"Você nasceu em um {week[datan]}")
 else:
     print(f"Você nasceu em uma {week[datan]}")
+
+#utilizando o metodo classico
+
+k = dian + (2 * mesn) + ((3 * (mesn + 1))/5) + anon + (anon/4) - (anon/100) + (anon/400) + 2 #Formula para calcular o dia da semana.
+
+diaf = int( k % 7 ) #Resto da divisão do valor
+
+semana = ("Sábado", "Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira")
+
+if diaf == 0 or diaf == 1:
+    print(f"Você nasceu em um {semana[diaf]}")
+else:
+    print(f"Você nasceu em uma {semana[diaf]}")

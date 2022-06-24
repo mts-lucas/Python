7f6d64aac305d3595eefe994fafe5c4bea8152ef
Lucas Mateus <lmateus1067@outlook.com>
2022-06-24 16:36:41 -0300

    codigo da prova

---
+++ Atividades Faculdade/segundaavaliacao.py
@@ -0,0 +1,13 @@
+import math
+
+print("Segunda avaliação\n")
+print("Questão 2\n")
+print("Calculo do quadrado de um numero")
+j = int(input("Por favor insira um numero: "))
+soma = 0
+for k in range(1, (j + 1)):
+    soma += (2 * k) - 1
+print(f"O quadrado de {j} é {soma}")
+
+
+print("Questão 3\n")

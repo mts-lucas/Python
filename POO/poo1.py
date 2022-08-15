dcac2a804b34ed3d4bbd07e5add636782e62202d
Lucas Mateus <lmateus1067@outlook.com>
2022-08-15 15:29:09 -0300

    finalizando primeira classe

--- POO/poo1.py
+++ POO/poo1.py
@@ -11,4 +11,49 @@ class Pessoa: #comando class para criar um molde, e por padrão é a primeira le
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
+    def comer(self, alimento):
+        if self.comendo:
+            print(f"{self.nome} já esta comendo")
+            return
+        elif self.falando:
+            print(f"{self.nome} não pode comer falando")
+            return
+
+        print(f"{self.nome} esta comendo {alimento}")
+        self.comendo = True
+
+    def pararcomer(self):
+        if self.comendo:
+            print(f'{self.nome} parou de comer')
+            self.comendo = False
+        else:
+            print(f"{self.nome} não está comendo")
+            return
+
+    def falar(self, assunto):
+        if self.comendo:
+            print(f"{self.nome} não pode falar comendo")
+            return
+        elif self.falando:
+            print(f"{self.nome} já esta falando")
+            return
         
+        print(f"{self.nome} esta falando sobre {assunto} ")
+        self.falando = True
+
+    def parardefalar(self):
+        if not self.falando:
+            print(f"{self.nome} não esta falando")
+            return
+        print(f"{self.nome} parou de falar")
+        self.falando = False
+
+pessoa1 = Pessoa("Lucas", 22)
+
+pessoa1.comer("Batata doce")
+pessoa1.pararcomer()
+pessoa1.comer("Ovo")
+pessoa1.falar("poo")
+pessoa1.pararcomer()
+pessoa1.falar("POO")
+pessoa1.parardefalar()

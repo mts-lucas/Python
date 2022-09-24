c6480b9560a06208e9252535901d231e6ed1f5ee
Lucas Mateus <lmateus1067@outlook.com>
2022-09-23 21:13:09 -0300

    sets

---
+++ Pessoal/estudo_sets.py
@@ -0,0 +1,27 @@
+# sets formam retiram elementos duplicados em iteraveis
+#set é um um conjunto que não é  acessavel por indices nem chaves como uma lista ou dic
+
+
+#forma menos comum de se usar:
+linguagens = {"Python", "Java", "Python"}
+print(linguagens)
+
+#forma mais comum
+linguagens3 = {"Python", "Java", "Python"}
+set(linguagens3)
+
+print(linguagens3)
+
+#para acessar valores de um set se pode transformar numa lista:
+
+teste = {3, 2, 5, 6, 9, 1 }
+
+# print(teste[0])  essa linha vai dar erro
+
+numeros = list(teste)
+print(numeros[0])
+
+# é possivel iterar um set usando for e para saber um indice podemos usar enumarate
+
+for indice, num in enumerate(numeros):
+    print(f"{indice}: {num}")

e7cb0c52561182efda68c3478295b5e6cd241a6b
Lucas Mateus <lmateus1067@outlook.com>
2022-10-09 14:08:56 -0300

    polimorfismo

---
+++ POO/polimorfismo.py
@@ -0,0 +1,32 @@
+# em herança eu posso ter o mesmo metodo se comportando de diferentes maneiras
+
+# exemplo: uma classe filha herda um metodo da classe mãe mas não nescessariamente usa daquela mesma forma
+
+class Passaro:
+    def voar(self):
+        print("Voando...")
+
+
+class Pardal(Passaro):
+    def voar(self):
+        print("Pardal pode voar")
+
+
+class Avestruz(Passaro):
+    def voar(self):
+        print("Avestruz não pode voar")
+
+
+# anotação : exemplo ruim do uso de herança para "ganhar" o método voar
+class Aviao(Passaro):
+    def voar(self):
+        print("Avião está decolando...")
+
+
+def plano_voo(obj):
+    obj.voar()
+
+
+plano_voo(Pardal())
+plano_voo(Avestruz())
+plano_voo(Aviao())

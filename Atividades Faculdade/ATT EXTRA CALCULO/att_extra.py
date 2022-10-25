47f2b4b18012feab814d8319d6d9afe9de8705cb
Lucas Mateus <lmateus1067@outlook.com>
2022-10-25 15:40:58 -0300

    resolver replace

--- Atividades Faculdade/ATT EXTRA CALCULO/att_extra.py
+++ Atividades Faculdade/ATT EXTRA CALCULO/att_extra.py
@@ -1,9 +1,9 @@
 # import numpy as np
 from numpy import*
-import sympy as sy
+# import sympy as sy
 # import matplotlib.pyplot as plt
 
-x = sy.symbol('x')
+# x = sy.symbol('x')
 # x = np.arange(1, 11)
 # print(x)
 
@@ -15,11 +15,22 @@ x = sy.symbol('x')
 
 
 def verifica_tvm(valora, valorb, equacao=str):
+
+    #calculando o ponto medio:
+
+    equacaoa = equacao
+    equacaob = equacao
+    funa = float(equacaoa.replace("x",  str(valora)))
+    funb = float(equacaob.replace("x",  str(valora)))
+    dervmed = (funb - funa)/(valorb - valora)
+
     constn = (valorb - valora) * 1000
     listax = linspace((valora + 0.001), (valorb - 0.001), num=constn)
     #para todo x dentro da lista
     for xs in listax:
         for char in equacao:
             if char == 'x':
-                equacao.replace("x", "xs")
-            
+                equacao.replace("x", str(xs))
+
+        if float(equacao) == dervmed:
+            pass

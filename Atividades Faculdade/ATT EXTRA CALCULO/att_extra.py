2c9019f09faab4f0e051ca094065de753b81cb41
Lucas Mateus <lmateus1067@outlook.com>
2022-10-26 00:09:23 -0300

    grafico falho

--- Atividades Faculdade/ATT EXTRA CALCULO/att_extra.py
+++ Atividades Faculdade/ATT EXTRA CALCULO/att_extra.py
@@ -1,7 +1,8 @@
 # import numpy as np
 from numpy import*
-# import sympy as sy
-# import matplotlib.pyplot as plt
+
+import sympy as sy
+import matplotlib.pyplot as plt
 
 # x = sy.symbol('x')
 # x = np.arange(1, 11)
@@ -13,37 +14,57 @@ from numpy import*
 # plt.plot(x, x+3, color='#f4d03f', label='f(x) = x + 3') # yellow
 
 
-
 def verifica_tvm(valora, valorb, equacao=str):
 
-    #calculando o ponto medio:
-    
-    equacaoa = equacao
-    equacaoa = equacaoa.replace("x", (str(valora)))
-    equacaob = equacao
-    equacaob = equacaob.replace("x", (str(valorb)))
-
+    # calculando o ponto medio:
+    equacaoa = equacao.replace("x", (str(valora)))
+    equacaob = equacao.replace("x", (str(valorb)))
     funa = float(eval(equacaoa))
     funb = float(eval(equacaob))
-    dervmed = (funb - funa)/(valorb - valora)
-    print(dervmed)
+    ponto_medio = round(((funb - funa)/(valorb - valora)), 5)
+    print(ponto_medio)
 
-    constn = (valorb - valora) * 1000
+    # definindo o vetor x
+    constn = (valorb - valora) * 10000
     listax = linspace(valora, valorb, num=constn)
     print(listax)
-    #para todo x dentro da lista
-    equacaofor = equacao
-    for xs in listax:  
+
+    # definindo a função derivada
+    x = sy.Symbol('x')
+    y = eval(equacao)
+    func_derivada = str(y.diff(x))
+    print(func_derivada)
+
+    listay = []
+    for xs in listax:
         # print(xs)
-        equacaofor = equacaofor.replace("x", (str(xs)))
+        equacaofor = func_derivada.replace("x", (str(xs)))
 
-        result = float(eval(equacaofor))
+        result = round((float(eval(equacaofor))), 5)
         # print(result)
-        if result == dervmed:
+        if (result - 0.0001 <= ponto_medio) and (result + 0.0001 >= ponto_medio):
+            pass
             print("ACHOU")
-            # print(result)
-            # break
-        else:     
-            equacaofor = equacao
+            print(result)
 
-verifica_tvm(2, 5, "x**(1/2)")
+        eqy = eval(equacao.replace("x", (str(valorb))))
+        listay.append(eqy)
+
+    #gerand graficos
+
+    xg = linspace(-15.0, 15.0)
+    yg = []
+    for xss in xg:
+        eqy1 = eval(equacao.replace("x", (str(xss))))
+        eqy = float(eqy1)
+        yg.append(eqy)
+    
+    # plt.plot(listax, y, color='#17a589', label='funcao')  # green
+    
+    plt.plot([valora, funa], [valorb, funb], color='#f4d03f', label='Derivada do Ponto C')
+    plt.plot(xg, yg, color='#17a589', label='Função') # green
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+verifica_tvm(-20, 10, "(4*(x**2)) + x + 1")

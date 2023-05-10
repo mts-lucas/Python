42a914327c166b53ca92394f1610e83646854a7e
Lucas Mateus <lmateus1067@outlook.com>
2023-05-10 00:12:48 -0300

    encerrando

--- Atividades Faculdade/algebra linear 1u/questa01.py
+++ Atividades Faculdade/algebra linear 1u/questa01.py
@@ -4,7 +4,7 @@ import numpy as np
 A = np.array([[1, 1, 1], [1, 2, 4], [1, 3, 9]])
 b = np.array([6, 15, 28])
 
-a = np.linalg.solve(A, b)
+a = np.linalg.solve(A, b)  # A.a = b
 
 print(f"a0 = {a[0]}\na1 = {a[1]}\na2 = {a[2]}")
 print("\nO polinômio interpolador é:\n")
--- Atividades Faculdade/algebra linear 1u/questao02.py
+++ Atividades Faculdade/algebra linear 1u/questao02.py
@@ -7,14 +7,18 @@ force = [0, 2.90, 14.8, 39.6, 74.3, 119]
 coefficientss = np.polyfit(velocity, force, 5)
 polynomial = np.poly1d(coefficientss)
 
-speedEstimated = 750
+speedEstimated = 7.5
 forceEstimated = polynomial(speedEstimated)
 
 print("O polinômio interpolador é: ")
 print(f"\np(t) = {coefficientss[0]:.3f} + {coefficientss[1]:.3f}t^1 + {coefficientss[2]:.3f}t^2 + {coefficientss[3]:.3f}t^3 + {coefficientss[4]:.3f}t^4 + {coefficientss[5]:.3f}t^5")  # noqa: E501
 
+print(f"forca estimada para 750 ft/sec de {forceEstimated:.2f} lb")
+
 plt.scatter(velocity, force, color='red', label='Medições')
 plt.plot(velocity, polynomial(velocity), label='Polinômio Interpolador')
+plt.plot(speedEstimated, forceEstimated, marker='o',
+         markersize=10, color='green', label='Força Estimada')
 plt.xlabel('Velocidade (100 ft/sec)')
 plt.ylabel('Força (100 lb)')
 plt.title('Polinômio Interpolador')

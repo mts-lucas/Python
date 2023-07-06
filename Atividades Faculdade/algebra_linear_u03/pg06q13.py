38d54264e216a0a5b3db14e48fd83d64c2a92998
Lucas Mateus <lmateus1067@outlook.com>
2023-07-06 12:16:18 -0300

    ref 0613

--- Atividades Faculdade/algebra_linear_u03/pg06q13.py
+++ Atividades Faculdade/algebra_linear_u03/pg06q13.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
 
 
 # Função que representa o modelo cúbico
@@ -13,19 +12,27 @@ t_data = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 y_data = np.array([0, 8.8, 29.9, 62.0, 104.7, 159.1, 222.0,
                   294.5, 380.4, 471.1, 571.7, 686.8, 809.2])
 
-# Encontrar os melhores valores de B0, B1, B2, B3
-optimized_params, _ = curve_fit(cubic_model, t_data, y_data)
-B0_optimized, B1_optimized, B2_optimized, B3_optimized = optimized_params
+# Perform matrix manipulation
+A = np.column_stack((np.ones_like(t_data), t_data, t_data**2, t_data**3))
+b = y_data
 
-# Criar uma série de pontos para plotar a curva ajustada
+# Calculate the parameters using the normal equation
+x = np.linalg.inv(A.T @ A) @ A.T @ b
+B0_optimized = x[0]
+B1_optimized = x[1]
+B2_optimized = x[2]
+B3_optimized = x[3]
+
+# Create a series of points to plot the fitted curve
 t_curve = np.linspace(min(t_data), max(t_data), 100)
 y_curve = cubic_model(t_curve, B0_optimized, B1_optimized,
                       B2_optimized, B3_optimized)
 
-# Calcular a velocidade quando t = 4.5 segundos
+# Calculate the velocity when t = 4.5 seconds
 t_estimate = 4.5
 velocity_estimate = cubic_model(
     t_estimate, B0_optimized, B1_optimized, B2_optimized, B3_optimized)
+
 print("Valores otimizados:")
 print("B0 =", B0_optimized)
 print("B1 =", B1_optimized)
@@ -33,9 +40,9 @@ print("B2 =", B2_optimized)
 print("B3 =", B3_optimized)
 print("Velocidade estimada quando t = 4.5 segundos:", velocity_estimate)
 
-# Plotar os dados observados e a curva ajustada
+# Plot the observed data and the fitted curve
 plt.scatter(t_data, y_data, label='Dados Observados')
-plt.scatter(4.5, velocity_estimate, label='velocidade esperada em 4.5s')
+plt.scatter(t_estimate, velocity_estimate, label='Velocidade estimada em 4.5s')
 plt.plot(t_curve, y_curve, 'r', label='Curva Ajustada')
 plt.xlabel('Tempo (t)')
 plt.ylabel('Posição (y)')
